6437db7e31d8182aa8006ca19666b01adfe8ddf5
chodone <89058129+chodone@users.noreply.github.com>
2021-08-26 20:21:35 +0900

    수업내용 추가

---
+++ js_advance/advance_01.js
@@ -0,0 +1,35 @@
+// html4
+console.log(document.getElementById("text1"));
+console.log(document.getElementsByClassName("text2"));
+console.log(document.getElementsByTagName("div"));
+
+// html5
+console.log(document.querySelector("#text1"));
+console.log(document.querySelector(".text2"));
+console.log(document.querySelector("div"));
+
+console.log(document.querySelectorAll(".text2"));
+console.log(document.querySelectorAll("div"));
+
+document.getElementById("text1").innerHTML = "Hello World";
+
+const para1 = document.createElement("p");
+const node1 = document.createTextNode("Hello");
+const element1 = para1.appendChild(node1);
+
+document.querySelectorAll(".text2")[0].appendChild(element1);
+
+// text2 class의 모두 html5 구문을 추가하고 싶을 때
+let text2Element = document.querySelectorAll(".text2");
+for (let i = 0; i < text2Element.length; i++) {
+  let para = document.createElement("p");
+  let node = document.createTextNode("Hello HTML 5");
+  let element = para.appendChild(node);
+  text2Element[i].appendChild(element);
+}
+
+// document.querySelector('.test2').appendChild('<P>')
+
+$(function () {
+  $(".text2").eq(0).append("<span>Append Jquery Node.</span>"); //eq() 인데스번호
+});
